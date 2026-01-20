0b231502298d73519b9366c1efcaa0910196ca0c
Wajahat Ullah <152395233+WajahatullahSE@users.noreply.github.com>
2026-01-20 19:23:35 +0500

    Update install_deps.sh

--- codedeploy/scripts/install_deps.sh
+++ codedeploy/scripts/install_deps.sh
@@ -5,7 +5,9 @@ set -e
 
 echo "Installing application dependencies..."
 
+sudo chown -R ec2-user:ec2-user /opt/nodeapp
 cd /opt/nodeapp
+npm ci --omit=dev
 
 # Install npm dependencies
 npm install --production
